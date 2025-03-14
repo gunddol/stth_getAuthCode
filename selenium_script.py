51e534d4cdcbd79565d999727e820df81dd082b4
gunddol <gunddol1004@hanyang.ac.kr>
2025-03-14 17:41:03 +0900

    upload cafe24

--- selenium_script.py
+++ selenium_script.py
@@ -6,6 +6,8 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from urllib.parse import urlparse, parse_qs
 import time
+import argparse
+import json
 
 def get_aws_keys(AWS_KEY_URL):
     response = requests.get(AWS_KEY_URL)
@@ -39,6 +41,54 @@ def upload_to_s3(code):
         print(f"✅ S3에 업로드 완료: s3://{S3_BUCKET_NAME}/{S3_FILE_NAME}")
     except Exception as e:
         print(f"❌ S3 업로드 실패: {str(e)}")
+
+
+def upload_cafe24(access_token, id, category, image_url):
+  try:
+    post_url = "https://suraktantan.cafe24api.com/api/v2/admin/boards/9/articles"
+    headers = {
+      "Authorization": f"Bearer {access_token}",
+      "Content-Type": "application/json",
+      "X-Cafe24-Api-Version": "2025-03-01"
+    }
+    post_data = {
+      "shop_no": 1,
+      "requests": [
+        {
+          "writer": id,
+          "title": "수락탄탄해 이미지 업로드",
+          "content": "contents",
+          "client_ip": "127.0.0.1",
+          "secret": "T",
+          "member_id": id,
+          "reply_user_id": "admin",
+          "reply_status": "N",
+          "category_no": category,
+          "attach_file_urls": [
+              {
+                  "name": image_url.split('/')[-1],
+                  "url": image_url
+              }
+          ]
+        }
+      ]
+    }
+    json_data = json.dumps(post_data)
+
+    post_response = requests.post(post_url, headers=headers, data=json_data)
+    post_response.raise_for_status()
+    post_response_data = post_response.json()
+    print(f"Post response: {post_response_data}")
+
+  except requests.exceptions.RequestException as e:
+    print(f"HTTP 요청 에러 발생: {e}")
+
+  except json.JSONDecodeError as e:
+    print(f"JSON 디코딩 에러 발생: {e}")
+    if 'post_response' in locals():
+      if hasattr(post_response, 'text'):
+        print(f"게시글 작성 응답 내용 (JSON 디코딩 에러 분석): {post_response.text}")
+
 
 def get_access_token(code):
   api_endpoint = "https://suraktantan.cafe24api.com/api/v2/oauth/token"
@@ -79,7 +129,7 @@ def create_driver():
     
     return driver
 
-def get_auth_code(driver):
+def get_auth_code(driver, id, category, image_url):
   try:
     target_url = "https://eclogin.cafe24.com/Shop/"
     driver.get(target_url)
@@ -105,15 +155,22 @@ def get_auth_code(driver):
 
     access_token = get_access_token(code)
     print("Access Token:", access_token)
-    upload_to_s3(access_token)
+    
+    upload_cafe24(access_token, id, category, image_url)
 
   finally:
     driver.quit()
 
 def main():
+  parser = argparse.ArgumentParser(description='Cafe24 API')
+  parser.add_argument('--id', type=str, required=True, help='id')
+  parser.add_argument('--category', type=str, required=True, help='category')
+  parser.add_argument('--image_url', type=str, required=True, help='image_url')
+  args = parser.parse_args()
+
   driver = create_driver()
   try:
-    get_auth_code(driver)
+    get_auth_code(driver, args.id, args.category, args.image_url)
   except Exception as e:
     print(f"❌ Error: {str(e)}")
   finally:
